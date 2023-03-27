98ec18568e7edc6491d0df4fedc07b19f66a892d
nomorechokedboy <haiduongit@protonmail.com>
2023-03-27 12:20:43 +0700

    feat(web): added active nav link for user sidebar

--- apps/web/app/projects/UserSidebar.tsx
+++ apps/web/app/projects/UserSidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { ReactNode } from 'react'
 import NavLink from './NavLink'
 import NavLinkLabel from './NavLinkLabel'
@@ -6,25 +7,33 @@ import NavLinkLabel from './NavLinkLabel'
 const userSidebarLinks: { label: ReactNode; path: string }[] = [
 	{
 		label: <NavLinkLabel>Users</NavLinkLabel>,
-		path: '/users'
+		path: ''
 	},
 	{
 		label: <NavLinkLabel>Groups</NavLinkLabel>,
-		path: '/users/groups'
+		path: 'groups'
 	},
 	{
 		label: <NavLinkLabel>Managed accounts</NavLinkLabel>,
-		path: '/users/accounts'
+		path: 'accounts'
 	}
 ]
 
 export default function UserSidebar() {
+	const pathname = usePathname()
+	const active = pathname.split('/').pop()
+
 	return (
 		<div className='pt-[16px]'>
 			{userSidebarLinks.map((el, i) => (
-				<Link href={el.path} key={i}>
+				<Link href={`/users/${el.path}`} key={i}>
 					<NavLink
 						className='text-[rgb(66,82,110)]'
+						active={
+							el.path === active ||
+							`/users/${el.path}` ===
+								pathname + '/'
+						}
 						{...el}
 					/>
 				</Link>
