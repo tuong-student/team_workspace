e0b74fb10d8b33394d7f983eee353b636c404352
nomorechokedboy <haiduongit@protonmail.com>
2023-03-30 22:50:41 +0700

    feat(api): implemented auth middleware

---
+++ apps/api/src/common/middleware.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"api/src/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	var token string
+	authHeader := c.Get("Authorization")
+
+	token = strings.TrimPrefix(authHeader, "Bearer ")
+	if token == "" {
+		return c.Status(http.StatusUnauthorized).JSON("Unauthorized")
+	}
+
+	userId, err := utils.ParseToken(token, []byte("jwtsec"))
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON("Internal error")
+	}
+
+	c.Locals("userId", userId)
+
+	return c.Next()
+}
