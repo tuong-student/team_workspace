0fe9f633227d3abc1cb16acb2bafda3b171efb42
nomorechokedboy <haiduongit@protonmail.com>
2023-03-31 23:23:28 +0700

    feat(web): updated swagger codegen

--- apps/web/libs/index.ts
+++ apps/web/libs/index.ts
@@ -1,8 +1,11 @@
-import axiosGlobal, { CreateAxiosDefaults } from 'axios'
-import { AuthApi, ProjectApi, UserApi } from '../codegen/api'
+import axiosGlobal, { AxiosError, CreateAxiosDefaults } from 'axios'
+import {
+	AuthApiFactory,
+	ProjectApiFactory,
+	UserApiFactory
+} from '../codegen/api'
 
-// const baseURL = 'https://api-fahasa-nomorechokedboy.cloud.okteto.net'
-const baseURL = 'http://localhost:3001'
+const baseURL = 'https://api-fahasa-nomorechokedboy.cloud.okteto.net'
 
 const configs: CreateAxiosDefaults = {
 	baseURL,
@@ -32,57 +35,75 @@ axios.interceptors.request.use((config: any) => {
 	}
 })
 
-export const auth = new AuthApi(undefined, undefined, axios)
-export const user = new UserApi(undefined, undefined, axios)
-export const project = new ProjectApi(undefined, undefined, axios)
+const createAxiosResponseInterceptor = () => {
+	const interceptor = axios.interceptors.response.use(
+		(response) => response,
+		async (error) => {
+			if (
+				error instanceof AxiosError &&
+				(error.response?.status !== 401 ||
+					!error.response.headers)
+			) {
+				// openNotification({ description, id, type })
+				return Promise.reject(error)
+			}
 
-// const createAxiosResponseInterceptor = () => {
-//     const interceptor = axios.interceptors.response.use(
-//         (response) => response,
-//         async (error) => {
-//             /* const id = crypto.randomUUID()
-//                 const type: NotificationMessage['type'] = 'error'
-//                 let description = 'Internet error' */
-//
-//             if (error.response?.data?.message) {
-//                 // description = error.response?.data?.message
-//             }
-//
-//             if (
-//                 error instanceof AxiosError &&
-//                 (error.response?.status !== 401 ||
-//                     !error.response.headers)
-//             ) {
-//                 // openNotification({ description, id, type })
-//                 return Promise.reject(error)
-//             }
-//
-//             axios.interceptors.response.eject(interceptor)
-//
-//             const token = useAuthToken()
-//             return auth
-//                 .authRefreshTokenPost({
-//                     refresh_token: token.value.refreshToken
-//                 })
-//                 .then((resp) => {
-//                     token.value.refreshToken =
-//                         resp.data.refresh_token
-//                     token.value.accessToken =
-//                         resp.data.token
-//                     error.response.config.headers[
-//                         'Authorization'
-//                     ] = `Bearer ${resp.data.token}`
-//                     return axios(error.response.config)
-//                 })
-//                 .catch((e) => {
-//                     token.value.accessToken = undefined
-//                     token.value.refreshToken = undefined
-//                     navigateTo('/login')
-//                     return Promise.reject(e)
-//                 })
-//                 .finally(createAxiosResponseInterceptor)
-//         }
-//     )
-// }
-//
-// createAxiosResponseInterceptor()
+			console.log('In refresh token')
+
+			axios.interceptors.response.eject(interceptor)
+
+			const refreshToken =
+				localStorage.getItem('refreshToken')
+			if (!refreshToken) {
+				console.warn('DEBUG')
+
+				return Promise.reject(Error('No refresh token'))
+			}
+
+			return auth
+				.authRefreshPost({ refreshToken })
+				.then(({ data }) => {
+					console.log('Lmao I am dead', data)
+
+					if (
+						!data.refreshToken ||
+						!data.accessToken
+					) {
+						throw Error('Server error')
+					}
+
+					localStorage.setItem(
+						'accessToken',
+						data.accessToken
+					)
+					localStorage.setItem(
+						'refreshToken',
+						data.refreshToken
+					)
+					error.response.config.headers[
+						'Authorization'
+					] = `Bearer ${data.accessToken}`
+
+					return axios(error.response.config)
+				})
+				.catch((e) => {
+					localStorage.removeItem('accessToken')
+					localStorage.removeItem('refreshToken')
+					return Promise.reject(e)
+				})
+				.finally(createAxiosResponseInterceptor)
+		}
+	)
+}
+
+createAxiosResponseInterceptor()
+
+const auth = AuthApiFactory(undefined, undefined, axios)
+const user = UserApiFactory(undefined, undefined, axios)
+const project = ProjectApiFactory(undefined, undefined, axios)
+
+export const $Api = {
+	auth,
+	user,
+	project
+}
