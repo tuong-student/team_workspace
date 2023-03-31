6b4159b0b2919145951372378d42dfe70c3be14b
nomorechokedboy <haiduongit@protonmail.com>
2023-03-31 10:02:09 +0700

    feat(api): removed hardcoded create admin

--- apps/api/src/user/export.go
+++ apps/api/src/user/export.go
@@ -2,8 +2,6 @@ package user
 
 import (
 	"api/src/common"
-	"api/src/utils"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jmoiron/sqlx"
@@ -22,30 +20,5 @@ func New(v1 fiber.Router) {
 
 func RegisterUserRepo(c *fiber.Ctx, db *sqlx.DB) {
 	Repo := &UserSqlxRepo{db: db}
-	createFirstAdmin(Repo)
 	c.Locals("UserRepo", Repo)
-}
-
-func createFirstAdmin(r *UserSqlxRepo) {
-	var total uint
-	if err := r.db.Get(&total, "SELECT COUNT(*) FROM users"); err != nil {
-		panic(fmt.Sprintf("Error count user: %s", err))
-	}
-
-	if total == 0 {
-		hashPassword, err := utils.GenerateFromPassword("admin123")
-		if err != nil {
-			panic(fmt.Sprintf("Can't hash first admin password: %s", err))
-		}
-
-		if _, err := r.
-			Insert(WriteUserBody{
-				FullName: "admin deep try",
-				Email:    "admin@gmail.com",
-				Password: *hashPassword,
-				Role:     "admin",
-			}); err != nil {
-			panic(fmt.Sprintf("Can't create first admin: %s", err))
-		}
-	}
 }
