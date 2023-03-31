6e2a730b41091e481cda7beb0c4b41950ac41197
nomorechokedboy <haiduongit@protonmail.com>
2023-03-31 15:50:47 +0700

    feat(web): updated projects layout

--- apps/web/app/projects/layout.tsx
+++ apps/web/app/projects/layout.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { usePathname } from 'next/navigation'
 import HeaderLayout from '../HeaderLayout'
 import SidebarLayout from '../SidebarLayout'
 import MainSidebar from './MainSidebar'
@@ -8,19 +9,22 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
+	const pathname = usePathname().split('/')
+	const Children =
+		pathname.length === 2 ? (
+			<>{children}</>
+		) : (
+			<SidebarLayout component={<MainSidebar />}>
+				{children}
+			</SidebarLayout>
+		)
 	return (
 		<html lang='en'>
 			<head>
 				<title>Projects</title>
 			</head>
 			<body className='flex h-screen w-screen flex-col'>
-				<HeaderLayout>
-					<SidebarLayout
-						component={<MainSidebar />}
-					>
-						{children}
-					</SidebarLayout>
-				</HeaderLayout>
+				<HeaderLayout>{Children}</HeaderLayout>
 			</body>
 		</html>
 	)
