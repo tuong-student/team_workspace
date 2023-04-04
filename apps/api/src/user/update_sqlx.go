37ecc53df9c93f8ca6f006f50ad3e70019028872
nomorechokedboy <haiduongit@protonmail.com>
2023-04-04 14:40:48 +0700

    feat(api): added role field to update user body

--- apps/api/src/user/update_sqlx.go
+++ apps/api/src/user/update_sqlx.go
@@ -9,14 +9,14 @@ import (
 
 var updateQuery = `
     UPDATE users
-    SET full_name = $2, avatar_url = $3
+    SET full_name = $2, avatar_url = $3, role = $4
     WHERE id = $1
-    RETURNING id, avatar_url, full_name, email, created_at, updated_at
+    RETURNING id, avatar_url, full_name, email, role, created_at, updated_at
 `
 
-func (r *UserSqlxRepo) Update(id uint, req UpdateUserBody) (*UserResp, error) {
-	var updatedUser UserResp
-	if err := r.db.QueryRowx(updateQuery, id, req.FullName, req.AvatarUrl).StructScan(&updatedUser); err != nil {
+func (r *UserSqlxRepo) Update(id uint, req UpdateUserBody) (*User, error) {
+	var updatedUser User
+	if err := r.db.QueryRowx(updateQuery, id, req.FullName, req.AvatarUrl, req.Role).StructScan(&updatedUser); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, common.ErrNotFound
 		}
