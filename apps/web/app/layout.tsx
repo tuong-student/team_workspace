59b805036165fc0990a4e53c6760fdb456286210
nomorechokedboy <haiduongit@protonmail.com>
2023-04-04 22:15:15 +0700

    fix(web): revert layout changes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,36 +1,21 @@
-/* 'use client'
+'use client'
 import './global.css'
 import NotificationList from './NotificationList'
 import Providers from './Providers'
 
 export default function RootLayout({
-    children
+	children
 }: {
-    children: React.ReactNode
+	children: React.ReactNode
 }) {
-    return (
-        <html lang='en'>
-            <head>
-                <title>Jira</title>
-            </head>
-            <body className='flex h-screen w-screen flex-col'>
-                <NotificationList />
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    )
-} */
-
-import { ReactNode } from 'react'
-
-export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en'>
 			<head>
 				<title>Jira</title>
 			</head>
 			<body className='flex h-screen w-screen flex-col'>
-				{children}
+				<NotificationList />
+				<Providers>{children}</Providers>
 			</body>
 		</html>
 	)
