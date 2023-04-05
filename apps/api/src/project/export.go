0a5bfb6877719a87c4e595b4bb7738218100448a
nomorechokedboy <haiduongit@protonmail.com>
2023-04-05 11:51:57 +0700

    feat(api): added admin check for endpoints which need admin role

--- apps/api/src/project/export.go
+++ apps/api/src/project/export.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"api/src/common"
+	"api/src/user"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jmoiron/sqlx"
@@ -11,14 +12,19 @@ func New(v1 fiber.Router) {
 	route := v1.Group("/project")
 
 	route.Use(common.AuthMiddleware)
-	route.Post("/create", CreateProject)
-	route.Delete("/delete/:id<int;min(1)>", DeleteProject)
-	route.Put("/update/:id<int;min(1)>", UpdateProject)
 	route.Get("/find", FindProject)
 	route.Get("/details/:id<int;min(1)>", FindOneProject)
+	registerIsAdminGuard(route)
 }
 
 func RegisterProjectRepository(c *fiber.Ctx, db *sqlx.DB) {
 	Repo := &ProjectSqlxRepo{db: db}
 	c.Locals("ProjectRepo", Repo)
+}
+
+func registerIsAdminGuard(route fiber.Router) {
+	route.Use(user.IsAdmin)
+	route.Post("/create", CreateProject)
+	route.Delete("/delete/:id<int;min(1)>", DeleteProject)
+	route.Put("/update/:id<int;min(1)>", UpdateProject)
 }
